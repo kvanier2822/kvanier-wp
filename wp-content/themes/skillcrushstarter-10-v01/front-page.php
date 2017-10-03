cc554d4f072d10a22fa61c3c0e604679406ceab1
khrukhru <khristian.vanier@wsu.edu>
2017-10-03 15:59:59 -0700

    added wordpress files and folders

---
+++ wp-content/themes/skillcrushstarter-10-v01/front-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * @package WordPress
+ * @subpackage Skillcrush_Starter
+ * @since Skillcrush Starter 2.0
+ */
+
+get_header(); ?>
+
+<section class="home-page">
+	<div class="main-content">
+		<div class="content">
+			<?php while ( have_posts() ): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
+					<a href="https://linkedin.com/in/kvanier" class="soc-icon ln"></a>
+					<a href="https://github.com/kvanier2822" class="soc-icon gh"></a>
+					<a href="instagram.com/lordtrashmouth" class="soc-icon in"></a>
+				</div>
+				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
